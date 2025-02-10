ea1b9e236f1bec98f931be5d0ca3aa178e704ae7
Alexandr Dorif <dorif11@gmail.com>
2025-02-10 13:57:28 +0200

    Updating docs.

--- docs/query_selector.py
+++ docs/query_selector.py
@@ -1,20 +1,18 @@
+# FATools uses YAML-formated query as its main query system.
 
-FATools uses YAML-formated query as its main query system.
-
-Example for samples:
+# Example for samples:
 
 samples:
-  S1:
-    - { batch: MYBATCH, codes: [ SAMPLE-001, SAMPLE-002 ] }
-    - { query: Indonesia[country] }
-    - { sample_ids: [ 102, 103, 104, 105] }
-  S2:
-    - { query: !Indonesia[country] }
+    S1:
+        - {batch: MYBATCH, codes: [SAMPLE-001, SAMPLE-002]}
+        - {query: Indonesia[country]}
+        - {sample_ids: [102, 103, 104, 105]}
+    S2:
+        - {query: !Indonesia[country]}
 
 
 Keys:
-batch
-codes
-query
-sample_ids
-
+    batch
+    codes
+    query
+    sample_ids
